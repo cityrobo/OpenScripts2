0330adf168f2a80c0c6785c4d7ffa7a25017879b
cityrobo <84413175+cityrobo@users.noreply.github.com>
2022-11-20 14:41:46 +0100

    Updated for U105 secondary magazine changes

--- OpenScripts2/src/MagazineScripts/UniversalMagazineGrabTrigger.cs
+++ OpenScripts2/src/MagazineScripts/UniversalMagazineGrabTrigger.cs
@@ -6,22 +6,36 @@ namespace OpenScripts2
 	public class UniversalMagazineGrabTrigger : FVRInteractiveObject
 	{
 		public FVRFireArm FireArm;
+		public bool IsSecondarySlotGrab;
+		public int SecondaryGrabSlot;
 
 		public override bool IsInteractable()
 		{
-			return FireArm.Magazine != null;
+			if (!IsSecondarySlotGrab)
+			{
+				return FireArm.Magazine != null;
+			}
+			return FireArm.SecondaryMagazineSlots[SecondaryGrabSlot].Magazine != null;
 		}
 
 		public override void BeginInteraction(FVRViveHand hand)
 		{
 			base.BeginInteraction(hand);
-			if (FireArm.Magazine != null)
+			if (!IsSecondarySlotGrab && FireArm.Magazine != null)
 			{
 				EndInteraction(hand);
 				FVRFireArmMagazine magazine = FireArm.Magazine;
 				FireArm.EjectMag(false);
 				hand.ForceSetInteractable(magazine);
 				magazine.BeginInteraction(hand);
+			}
+			else if (IsSecondarySlotGrab && FireArm.SecondaryMagazineSlots[SecondaryGrabSlot].Magazine != null)
+			{
+				EndInteraction(hand);
+				FVRFireArmMagazine magazine2 = FireArm.SecondaryMagazineSlots[SecondaryGrabSlot].Magazine;
+				FireArm.EjectSecondaryMagFromSlot(SecondaryGrabSlot, false);
+				hand.ForceSetInteractable(magazine2);
+				magazine2.BeginInteraction(hand);
 			}
 		}
 		/*
