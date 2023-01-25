3873bae7dc66af2c92b319667ffe5bbbfdddcf3d
cityrobo <84413175+cityrobo@users.noreply.github.com>
2023-01-25 21:03:48 +0100

    new internal magazine MagazineCutoff functionality added for bolt actions and such

--- OpenScripts2/src/MagazineScripts/MagazineCutoff.cs
+++ OpenScripts2/src/MagazineScripts/MagazineCutoff.cs
@@ -1,7 +1,9 @@
 using FistVR;
+using MonoMod.Cil;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -22,6 +24,9 @@ namespace OpenScripts2
         public OpenScripts2_BasePlugin.TransformType MovementType;
         public OpenScripts2_BasePlugin.Axis Axis;
 
+        public bool MakesMagazineUnaccessibleOnActive = false;
+        public bool MakesChamberAccessibleOnActive = false;
+
         [Header("Sound")]
         public AudioEvent Sounds;
 
@@ -38,14 +43,107 @@ namespace OpenScripts2
         private Quaternion _targetRotation;
         private Vector3 _targetPosition;
 
+        private bool _wasClipFed = false;
+
+        private static List<FVRFireArm> s_firearmsWithMagazineCutoff = new();
+
+#if !DEBUG
+        static MagazineCutoff()
+        {
+            On.FistVR.FVRFireArmRound.GetNumRoundsPulled += FVRFireArmRound_GetNumRoundsPulled;
+            On.FistVR.FVRFireArmRound.DuplicateFromSpawnLock += FVRFireArmRound_DuplicateFromSpawnLock;
+        }
+
+        private static GameObject FVRFireArmRound_DuplicateFromSpawnLock(On.FistVR.FVRFireArmRound.orig_DuplicateFromSpawnLock orig, FVRFireArmRound self, FVRViveHand hand)
+        {
+            GameObject gO = orig(self, hand);
+            FVRFireArmRound component = gO.GetComponent<FVRFireArmRound>();
+            if (GM.Options.ControlOptions.SmartAmmoPalming == ControlOptions.SmartAmmoPalmingMode.Enabled && component != null && hand.OtherHand.CurrentInteractable != null)
+            {
+                int num = 0;
+                if (hand.OtherHand.CurrentInteractable is FVRFireArm fireArm && s_firearmsWithMagazineCutoff.Contains(fireArm))
+                {
+                    if (fireArm.RoundType == self.RoundType)
+                    {
+                        FVRFireArmMagazine magazine = fireArm.Magazine;
+                        if (magazine != null && magazine.IsDropInLoadable)
+                        {
+                            num = magazine.m_capacity - magazine.m_numRounds;
+                        }
+                        for (int i = 0; i < fireArm.GetChambers().Count; i++)
+                        {
+                            FVRFireArmChamber fvrfireArmChamber = fireArm.GetChambers()[i];
+                            if (fvrfireArmChamber.IsManuallyChamberable && (!fvrfireArmChamber.IsFull || fvrfireArmChamber.IsSpent))
+                            {
+                                num++;
+                            }
+                        }
+                    }
+                    component.DestroyAllProxies();
+                    if (num < 1)
+                    {
+                        num = self.ProxyRounds.Count;
+                    }
+                    int num2 = Mathf.Min(self.ProxyRounds.Count, num - 1);
+                    for (int k = 0; k < num2; k++)
+                    {
+                        component.AddProxy(self.ProxyRounds[k].Class, self.ProxyRounds[k].ObjectWrapper);
+                    }
+                }
+                component.UpdateProxyDisplay();
+            }
+
+            return gO;
+        }
+
+        private static int FVRFireArmRound_GetNumRoundsPulled(On.FistVR.FVRFireArmRound.orig_GetNumRoundsPulled orig, FVRFireArmRound self, FVRViveHand hand)
+        {
+            int num = 0;
+            if (hand.OtherHand.CurrentInteractable is FVRFireArm fireArm && s_firearmsWithMagazineCutoff.Contains(fireArm))
+            {
+                if (fireArm.RoundType == self.RoundType)
+                {
+                    FVRFireArmMagazine magazine = fireArm.Magazine;
+                    if (magazine != null && magazine.IsDropInLoadable)
+                    {
+                        num = magazine.m_capacity - magazine.m_numRounds;
+                    }
+                    for (int i = 0; i < fireArm.GetChambers().Count; i++)
+                    {
+                        FVRFireArmChamber fvrfireArmChamber = fireArm.GetChambers()[i];
+                        if (fvrfireArmChamber.IsManuallyChamberable && (!fvrfireArmChamber.IsFull || fvrfireArmChamber.IsSpent))
+                        {
+                            num++;
+                        }
+                    }
+                }
+                return num;
+            } 
+            else return orig(self, hand);
+        }
+#endif
         public override void Awake()
         {
-            base.Start();
+            base.Awake();
+
+            s_firearmsWithMagazineCutoff.Add(FireArm);
 
             IsSimpleInteract = true;
             CalculatePositions();
 
+            _targetPosition = _startPos;
+            _targetRotation = _startRot;
+
             _mag = FireArm.Magazine;
+
+            _wasClipFed = FireArm.UsesClips;
+        }
+
+        public override void OnDestroy()
+        {
+            s_firearmsWithMagazineCutoff.Remove(FireArm);
+
+            base.OnDestroy();
         }
 
         public override void SimpleInteraction(FVRViveHand hand)
@@ -78,20 +176,57 @@ namespace OpenScripts2
                 if (_mag.FireArm == FireArm)
                 {
                     _mag.IsExtractable = false;
+                    if (MakesMagazineUnaccessibleOnActive)
+                    {
+                        _mag.IsDropInLoadable = false;
+                        if (_wasClipFed)
+                        {
+                            FireArm.UsesClips = false;
+                            FireArm.ClipTrigger.SetActive(false);
+                        }
+                    }
+
+                    if (MakesChamberAccessibleOnActive)
+                    {
+                        List<FVRFireArmChamber> chambers = FireArm.GetChambers();
+                        foreach (FVRFireArmChamber chamber in chambers)
+                        {
+                            chamber.IsAccessible = true;
+                        }
+                    }
                 }
                 else
                 {
                     _mag.IsExtractable = true;
+                    if (MakesMagazineUnaccessibleOnActive) _mag.IsDropInLoadable = true;
                     _mag = null;
                 }
             }
             else if (!_MagazineCuttoffActive && _mag != null)
             {
                 _mag.IsExtractable = true;
+                if (MakesMagazineUnaccessibleOnActive)
+                {
+                    _mag.IsDropInLoadable = true;
+                    if (_wasClipFed)
+                    {
+                        FireArm.UsesClips = true;
+
+                        if (FireArm is BoltActionRifle boltActionRifle && boltActionRifle.CurBoltHandleState == BoltActionRifle_Handle.BoltActionHandleState.Rear) FireArm.ClipTrigger.SetActive(true);
+                    }
+                }
+
+                if (MakesChamberAccessibleOnActive)
+                {
+                    List<FVRFireArmChamber> chambers = FireArm.GetChambers();
+                    foreach (FVRFireArmChamber chamber in chambers)
+                    {
+                        chamber.IsAccessible = false;
+                    }
+                }
             }
 
             _mag = FireArm.Magazine;
-
 
             if (MovementType == OpenScripts2_BasePlugin.TransformType.Rotation && CutoffLever.localRotation != _targetRotation)
             {
@@ -102,7 +237,6 @@ namespace OpenScripts2
                 CutoffLever.localPosition = Vector3.MoveTowards(CutoffLever.localPosition, _targetPosition, Speed * Time.deltaTime);
             }
         }
-
 
         private void CalculatePositions()
         {
